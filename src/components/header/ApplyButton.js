050993ce45cb97a7e49adfe4c7192c24d4485c4c
Kseniia P <ksenia.pidopryhora@gmail.com>
2022-12-15 21:42:26 +0200

    update Header

---
+++ src/components/header/ApplyButton.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Header.scss";
+
+const ApplyButton = () => {
+  return (
+    <button className="btn__apply">Apply</button>
+  )
+}
+
+export default ApplyButton
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,44 +2,56 @@ import "./Header.scss";
 import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import Logo from "./Logo";
+import ApplyButton from "./ApplyButton";
 
 import Burber from "../../assets/burger.svg";
-
 
 const Header = () => {
   const [click, setclick] = useState(false);
   const handleClick = () => setclick(!click);
 
+
+
   return (
-    <div className="header">
-      <a href="#">
-        <Logo />
-      </a>
-      <nav className={click ? "nav__menu active" : "nav__menu"}>
-        <ul className="nav__list">
-          <li>
-            <a href="#" className="active">Home</a>
-          </li>
-          <li>
-            <a href="#">Our mission</a>
-          </li>
-          <li>
-            <a href="#">Places</a>
-          </li>
-          <li>
-            <a href="#">Team</a>
-          </li>
-        </ul>
-        <button className="btn__apply">Apply</button>
-      </nav>
-      <div className="hamburger" onClick={handleClick}>
-        {click ? (
-          <FaTimes size={20} style={{ color: "#000" }} />
-        ) : (
-          <FaBars size={20} style={{ color: "#000" }} />
-        )}
+    <header className="header">
+      <div className="header__container">
+        <a href="#">
+          <Logo />
+        </a>
+        <nav className={click ? "nav__menu active" : "nav__menu"}>
+          <ul className="nav__list">
+            <li>
+              <a href="#" onClick={handleClick}>
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" onClick={handleClick}>Our mission</a>
+            </li>
+            <li>
+              <a href="#" onClick={handleClick}>Places</a>
+            </li>
+            <li>
+              <a href="#" onClick={handleClick}>Team</a>
+            </li>
+          </ul>
+          <div className="desktopHidden" >
+            <ApplyButton />
+          </div>
+        </nav>
+        <div className="mobileHidden">
+          <ApplyButton />
+        </div>
+
+        <div className="hamburger" onClick={handleClick}>
+          {click ? (
+            <FaTimes size={20} style={{ color: "#000" }} />
+          ) : (
+            <FaBars size={20} style={{ color: "#000" }} />
+          )}
+        </div>
       </div>
-    </div>
+    </header>
   );
 };
 
