34c9aa77507a71d9982814a9adc89ab7b4cba648
Kseniia P <ksenia.pidopryhora@gmail.com>
2022-12-15 16:47:40 +0200

    update Header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,34 +1,46 @@
-import './Header.scss'
-import React, { useState } from 'react'
-import { FaBars, FaTimes } from 'react-icons/fa'
-import Logo from './Logo';
+import "./Header.scss";
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import Logo from "./Logo";
+
+import Burber from "../../assets/burger.svg";
+
 
 const Header = () => {
-    const [click, setclick] = useState(false);
-    const handleClick = () => setclick(!click)
+  const [click, setclick] = useState(false);
+  const handleClick = () => setclick(!click);
 
-    return (
-        <div className='header'>
-            <Logo />
-            <ul className={click ? "nav__menu active" : "nav__menu"}>
-                <li>
-                    <a href='#'>Home</a>
-                </li>
-                <li>
-                    <a href='#'>Our mission</a>
-                </li>
-                <li>
-                    <a href='#'>Places</a>
-                </li>
-                <li>
-                    <a href='#'>Team</a>
-                </li>
-            </ul>
-            <div className="hamburger" onClick={handleClick}>
-                {click ? (<FaTimes size={20} style={{ color: '#000' }} />) : ((<FaBars size={20} style={{ color: '#000' }} />))}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="header">
+      <a href="#">
+        <Logo />
+      </a>
+      <nav className={click ? "nav__menu active" : "nav__menu"}>
+        <ul className="nav__list">
+          <li>
+            <a href="#" className="active">Home</a>
+          </li>
+          <li>
+            <a href="#">Our mission</a>
+          </li>
+          <li>
+            <a href="#">Places</a>
+          </li>
+          <li>
+            <a href="#">Team</a>
+          </li>
+        </ul>
+        <button className="btn__apply">Apply</button>
+      </nav>
+      <div className="hamburger" onClick={handleClick}>
+        {click ? (
+          <FaTimes size={20} style={{ color: "#000" }} />
+        ) : (
+          <FaBars size={20} style={{ color: "#000" }} />
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
--- src/components/header/Logo.js
+++ src/components/header/Logo.js
@@ -7,18 +7,33 @@ import logo3 from "../../assets/logo3.svg";
 import Rootz from "../../assets/Rootz.svg";
 
 const Logo = () => {
-    return (
-        <div className={styles.logo__wrapper}>
-            <div className={styles.logoImg}>
-                <img className={`${styles.logoImg__item} ${styles.item1}`} src={logo1} alt="" />
-                <img className={`${styles.logoImg__item} ${styles.item2}`} src={logo2} alt="" />
-                <img className={`${styles.logoImg__item} ${styles.item3}`} src={logo3} alt="" />
-            </div>
-            <div className={styles.logoName}>
-
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.logo__wrapper}>
+      <div className={styles.logoImg}>
+        <img
+          className={`${styles.logoImg__item} ${styles.item1}`}
+          src={logo1}
+          alt=""
+        />
+        <img
+          className={`${styles.logoImg__item} ${styles.item2}`}
+          src={logo2}
+          alt=""
+        />
+        <img
+          className={`${styles.logoImg__item} ${styles.item3}`}
+          src={logo3}
+          alt=""
+        />
+      </div>
+      <div className={styles.logoName}>
+        <img
+          src={Rootz}
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Logo;
