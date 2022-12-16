61b04d649df80d8743e90bf55dcca8761b33eab4
Kseniia P <ksenia.pidopryhora@gmail.com>
2022-12-17 00:09:58 +0200

    center Login

--- src/components/Main/Login/Login.js
+++ src/components/Main/Login/Login.js
@@ -14,21 +14,19 @@ const Login = () => {
     }
 
     return (
-        <div className='container'>
-            <div className="login">
-                <div className="loginLeft">
-                    <h2>Get started today!</h2>
-                    <p>Learn more about how you can save our planet's nature. From smart consumption to switching to renewable energy, each of us can do our part to save the planet.</p>
-                </div>
-
-                <form action="#" method="post" encType="multipart/form-data">
-                    <h3>Log in</h3>
-                    <input type="text" placeholder='Name' value={name} onChange={handleName} />
-                    <input type="text" placeholder='Email' value={email} onChange={handleEmail} />
-                    <button className="btn" type='submit'>Book a demo</button>
-                </form>
-
+        <div className="login">
+            <div className="loginLeft">
+                <h2>Get started today!</h2>
+                <p>Learn more about how you can save our planet's nature. From smart consumption to switching to renewable energy, each of us can do our part to save the planet.</p>
             </div>
+
+            <form action="#" method="post" encType="multipart/form-data">
+                <h3>Log in</h3>
+                <input type="text" placeholder='Name' value={name} onChange={handleName} />
+                <input type="text" placeholder='Email' value={email} onChange={handleEmail} />
+                <button className="btn" type='submit'>Book a demo</button>
+            </form>
+
         </div>
     )
 }
