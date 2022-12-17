a86b0474aedf38670c83652f04d575730f0504c9
Kseniia P <ksenia.pidopryhora@gmail.com>
2022-12-17 06:19:33 +0200

    add Slider component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,18 +3,20 @@ import Banner from "./Banner/Banner";
 import GetStarted from "./GetStarted/GetStarted";
 import Login from "./Login/Login";
 import "./Main.scss";
+import Slider from "./Slider.js/Slider";
 import Team from "./Team/Team";
 
 const Main = () => {
   return (
     <main className="main">
-        <Banner />
+      <Banner />
       <div className="container">
         <Team />
       </div>
       <Login />
       <div className="container">
         <GetStarted />
+        <Slider />
       </div>
     </main>
   );
